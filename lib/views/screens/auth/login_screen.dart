cef7a77a33a8b84e739d92d37f3d4a9f89e67bd2
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-02 20:06:57 +0600

    login and registration screen ui

---
+++ lib/views/screens/auth/login_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
+
+import '../../../widgets/input_text.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+TextEditingController emailTextEditingController = TextEditingController();
+TextEditingController passwordEditingController = TextEditingController();
+bool showprogressBar = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 200,
+                ),
+               const Text(
+                  "Login Screen",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22.0,
+                    letterSpacing: 4
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: InputTextWidget(
+                    textEditingController: emailTextEditingController,
+                    labelString: "Email",
+                    iconData: Icons.email_outlined,
+                    isObscure: false,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  child: InputTextWidget(
+                    textEditingController: passwordEditingController,
+                    labelString: "Password",
+                    iconData: Icons.lock,
+                    isObscure: true,
+                  ),
+                ),
+                const SizedBox(height: 30,),
+                showprogressBar == false?
+                Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width - 50,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: InkWell(
+                        onTap: (){
+                          setState(() {
+                            showprogressBar = true;
+                          });
+                        },
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ):const SimpleCircularProgressBar(
+                  progressColors: [
+                    Colors.green,
+                    Colors.blue,
+                    Colors.red,
+                    Colors.amber,
+                    Colors.blueAccent
+                  ],
+                  backColor: Colors.white30,
+                  animationDuration: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Not have an Account? ",style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16
+                    ),),
+                    InkWell(
+                      onTap: ()
+                      {
+
+                      },
+                      child: const Text("SignUp Now",style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                      ),),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/auth/registration_screen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
+
+import '../../../widgets/input_text.dart';
+import 'login_screen.dart';
+
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  TextEditingController userNameEditingController = TextEditingController();
+  TextEditingController emailEditingController = TextEditingController();
+  TextEditingController passwordEditingController = TextEditingController();
+  bool showProgessBar = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 200,
+              ),
+
+              const SizedBox(
+                height: 15,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: const CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.black,
+                  backgroundImage:
+                  AssetImage("assets/images/profile_avatar.jpg"),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: userNameEditingController,
+                  labelString: 'Username',
+                  isObscure: false,
+                  iconData: Icons.person_outline,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: userNameEditingController,
+                  labelString: 'Email',
+                  isObscure: false,
+                  iconData: Icons.email_outlined,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: userNameEditingController,
+                  labelString: 'Password',
+                  isObscure: true,
+                  iconData: Icons.lock_outline,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              showProgessBar == false
+                  ? Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 38,
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          showProgessBar = true;
+                        });
+                      },
+                      child: const Center(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+                  : const SimpleCircularProgressBar(
+                progressColors: [
+                  Colors.green,
+                  Colors.blue,
+                  Colors.red,
+                  Colors.amber,
+                  Colors.blueAccent
+                ],
+                backColor: Colors.white30,
+                animationDuration: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an Account? ",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){
+                      Get.to(LoginScreen());
+                    },
+                    child: const Text(
+                      "Login Now",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
