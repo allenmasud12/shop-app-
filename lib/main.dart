87a7475e6131b266c9e5b39d2422e852c06b2770
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-09 15:17:20 +0600

    firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:shop_app/views/screens/auth/login_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
