4ae03a7656f733cf1c3fce281bd8cf566baec7e8
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-09 20:15:46 +0600

    firebase fire store user data

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,15 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<String> createNewUser(
-      String username, String email, String password) async {
+  Future<String> createNewUser(String username, String email, String password) async {
     String res = "some error";
 
     try {
-      await _auth.createUserWithEmailAndPassword(
+     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
+      await _firestore.collection("buyers").doc(userCredential.user!.uid).set({
+        'username': username,
+        'email': email,
+        'buyerId': userCredential.user!.uid,
+      });
+
       res = 'success';
     } catch (e) {
       res = e.toString();
