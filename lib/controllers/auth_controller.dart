e5ef73cdb6842806417626556a4b8faeaabffefa
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-06-10 13:47:03 +0600

    profile image added and upload firebase

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,22 +1,61 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:typed_data';
 
 class AuthController {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<String> createNewUser(String username, String email, String password) async {
+  /// function to select image from gallery or camera
+  Future<Uint8List?> pickProfileImage(ImageSource source) async {
+    final ImagePicker _imagePicker = ImagePicker();
+
+    XFile? file = await _imagePicker.pickImage(source: source);
+
+    if (file != null) {
+      return await file.readAsBytes();
+    } else {
+      print("No image selected");
+      return null;
+    }
+  }
+
+  /// upload image
+  Future<String> uploadImageToStorage(Uint8List? image) async {
+    if (image == null) {
+      throw ArgumentError("Image data cannot be null");
+    }
+
+    Reference ref = _storage.ref().child("profileImages").child(_auth.currentUser!.uid);
+    UploadTask uploadTask = ref.putData(image);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  /// user auth registration
+  Future<String> createNewUser(String username, String email, String password, Uint8List? image) async {
     String res = "some error";
 
     try {
-     UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
+
+      String? photoUrl;
+      if (image != null) {
+        photoUrl = await uploadImageToStorage(image);
+      }
+
       await _firestore.collection("buyers").doc(userCredential.user!.uid).set({
         'username': username,
         'email': email,
         'buyerId': userCredential.user!.uid,
+        'photoUrl': photoUrl,
       });
-
       res = 'success';
     } catch (e) {
       res = e.toString();
--- lib/views/screens/auth/registration_screen.dart
+++ lib/views/screens/auth/registration_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shop_app/controllers/auth_controller.dart';
 import 'package:simple_circular_progress_bar/simple_circular_progress_bar.dart';
+import 'dart:typed_data';
 
 import '../../../widgets/input_text.dart';
 import 'login_screen.dart';
@@ -21,6 +23,22 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final AuthController _authController = AuthController();
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
+  Uint8List? _image;
+
+  Future<void> galleryImage() async {
+    Uint8List? im = await _authController.pickProfileImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  Future<void> captureImage() async {
+    Uint8List? im = await _authController.pickProfileImage(ImageSource.camera);
+    setState(() {
+      _image = im;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +51,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 height: 30,
               ),
               GestureDetector(
-                onTap: () {},
-                child: const CircleAvatar(
+                onTap: galleryImage,
+                child: _image == null
+                    ? const CircleAvatar(
                   radius: 80,
                   backgroundColor: Colors.black,
                   backgroundImage:
-                      AssetImage("assets/images/profile_avatar.jpg"),
+                  AssetImage("assets/images/profile_avatar.jpg"),
+                )
+                    : CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.black,
+                  backgroundImage: MemoryImage(_image!),
                 ),
               ),
               const SizedBox(
@@ -88,55 +112,62 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ),
               showProgessBar == false
                   ? Column(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width - 38,
-                          height: 54,
-                          decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: InkWell(
-                            onTap: () {
-                              if (_formkey.currentState!.validate()) {
-                                setState(() {
-                                  showProgessBar = true;
-                                });
-                                _authController.createNewUser(
-                                    userNameEditingController.text,
-                                    emailEditingController.text ,
-                                    passwordEditingController.text,
-                                );
-                                print(userNameEditingController);
-                                print(emailEditingController);
-                                print(passwordEditingController);
-                              } else {
-                                print("not valid");
-                              }
-                            },
-                            child: const Center(
-                              child: Text(
-                                "Sign Up",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  : const SimpleCircularProgressBar(
-                      progressColors: [
-                        Colors.green,
-                        Colors.blue,
-                        Colors.red,
-                        Colors.amber,
-                        Colors.blueAccent
-                      ],
-                      backColor: Colors.white70,
-                      animationDuration: 5,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width - 38,
+                    height: 54,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: InkWell(
+                      onTap: () async {
+                        if (_formkey.currentState!.validate()) {
+                          setState(() {
+                            showProgessBar = true;
+                          });
+                          String res = await _authController.createNewUser(
+                            userNameEditingController.text,
+                            emailEditingController.text,
+                            passwordEditingController.text,
+                            _image,
+                          );
+                          setState(() {
+                            showProgessBar = false;
+                          });
+
+                          if (res == 'success') {
+                            Get.to(() => LoginScreen());
+                          } else {
+                            print("Error: $res");
+                          }
+                        } else {
+                          print("not valid");
+                        }
+                      },
+                      child: const Center(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
                     ),
+                  )
+                ],
+              )
+                  : const SimpleCircularProgressBar(
+                progressColors: [
+                  Colors.green,
+                  Colors.blue,
+                  Colors.red,
+                  Colors.amber,
+                  Colors.blueAccent
+                ],
+                backColor: Colors.white70,
+                animationDuration: 5,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -149,7 +180,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   ),
                   InkWell(
                     onTap: () {
-                      Get.to(LoginScreen());
+                      Get.to(() => LoginScreen());
                     },
                     child: const Text(
                       "Login Now",
