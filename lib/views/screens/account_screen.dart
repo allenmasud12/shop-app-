946ebc22d3e2680281713eac9ed4115973e422e2
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-07-05 22:57:20 +0600

    profile page ui

--- lib/views/screens/account_screen.dart
+++ lib/views/screens/account_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({super.key});
@@ -6,9 +7,112 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("account screen"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            "Profile",
+            style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 4,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.sunny_snowing,
+                color: Colors.pink,
+              ),
+            )
+          ],
+          centerTitle: true,
+          backgroundColor: primaryColor,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                15.height,
+                CircleAvatar(
+                  radius: 65,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Test Name",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "test@gmail.com",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 4,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: (){
+          
+                    },
+                    child: Text("Edit Profile"),
+                  ),
+                ),
+                Divider(thickness: 2,color: Colors.grey,),
+          
+             ListTile(
+               leading: Icon(Icons.settings),
+               title: Text('Settings'),
+               onTap: () {
+          
+                  },
+          ),
+          
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text('Phone'),
+                  subtitle: Text("+88016901645441"),
+                  onTap: () {
+          
+                  },
+                ),
+          
+                ListTile(
+                  leading: Icon(Icons.shopping_bag),
+                  title: Text('Cart'),
+                  onTap: () {
+          
+                  },
+                ),
+          
+                ListTile(
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Orders'),
+                  onTap: () {
+          
+                  },
+                ),
+
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('Logout',style: TextStyle(color: Colors.red),),
+                  onTap: () {
+
+                  },
+                ),
+          
+              ],
+            ),
+          ),
+        ));
   }
 }
